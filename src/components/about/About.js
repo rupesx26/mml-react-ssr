3a5c554729d89d72d7b10aff773c3bdf3807b4a7
Rupesh Mayekar <rupeshmayekar@Rupeshs-MacBook-Air.local>
2021-11-27 18:21:01 +0530

    check url and port

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { imagePath } from '../../utils/assetUtils';
-import styles from './about.module.scss';
-
+import { imagePath } from "../../utils/assetUtils";
+import styles from "./about.module.scss";
+console.log("process.env.PUBLIC_URL", process.env.PUBLIC_URL, process.env.PORT);
 class About extends Component {
   render() {
     return (
       <div>
         <h1 className={styles.title}>About page</h1>
-        <img className={styles.reactLogo} src={imagePath('react.svg')} alt="" />
+        <img className={styles.reactLogo} src={imagePath("react.svg")} alt="" />
       </div>
     );
   }
